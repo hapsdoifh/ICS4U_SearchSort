a21984d9e745ae139282edf65cf515eb697bc835
HarryWang <50002801+hapsdoifh@users.noreply.github.com>
2022-11-27 10:29:43 -0500

    Completed basic function Some types are perhaps inappropriate for the purpose of the program might need to change those

--- SecondProgram/Main.java
+++ SecondProgram/Main.java
@@ -1,9 +1,24 @@
 package SecondProgram;
 
 import SecondProgram.Temperature.temps;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
         //DO ADD/CHANGE how the list is inputted
+        Scanner keyboard = new Scanner(System.in);
+        double[] j = {19,20.4,49.1,16.3,13.6};
+        Temperature myTemp = new Temperature(j);
+        String inpt = "";
+        //while(myTemp.inputTemp());
+        double [][] k = myTemp.convertList(myTemp.SelectionSort(false));
+        for(double[] outpt : k){         
+            for(double dbOut : outpt){
+                System.out.print(dbOut + "\t");
+            }   
+            System.out.println();
+        }
+        System.out.print(myTemp.BinarySearch(myTemp.SelectionSort(false),16.3)+1);
+
     }
 }
--- SecondProgram/Temperature.java
+++ SecondProgram/Temperature.java
@@ -2,48 +2,81 @@ package SecondProgram;
 import java.util.*;
 
 import javax.swing.text.StyledEditorKit.BoldAction;
+import javax.xml.transform.Templates;
 
 public class Temperature {
     class temps{
         public double temp;
-        public double day;
-        public temps(double t, double d){
+        public int  day;
+        public temps(double t, int d){
             temp = t;
             day = d;
         }
     }
     private ArrayList<temps> tempList = new ArrayList<temps>();
-    
-    public void inputTemp(){
+    public Temperature(double[] tps){
+        for(int i = 0; i<tps.length; i++){
+            tempList.add(new temps(tps[i],i));
+        }
+    }
+    public boolean inputTemp(){
         Scanner sc = new Scanner(System.in);
-        double t,d;
-        System.out.println("Temperature: ");
-        t = sc.nextDouble();
-        System.out.println("Day number: ");
-        d = sc.nextDouble();
-        tempList.add(new temps(t, d));
+        String t,d;
+        System.out.println();
+        System.out.print("Temperature (q to quit): ");
+        t = sc.nextLine();
+        if(t.equals("q")){
+            return false;
+        }
+        System.out.print("Day number (q to quit): ");
+        d = sc.nextLine();
+        if(d.equals("q")){
+            return false;
+        }
+        tempList.add(new temps(Double.parseDouble(t), Integer.parseInt(d)));
+        return true;
     }
 
     public ArrayList<temps> SelectionSort(boolean order){
         ArrayList<temps> SortedList = new ArrayList<temps>();
+        for(temps cp : tempList){
+            SortedList.add(new temps(cp.temp, cp.day));
+        }
         temps TransferStore;
         int minInd = 0;
-        double min = tempList.get(minInd).temp;
-        for(int i=0; i<tempList.size(); i++){           
-            for(int k = i; k < tempList.size(); k++){
-                if(tempList.get(k).temp < min){
-                    min = tempList.get(k).temp;
+        double min = SortedList.get(minInd).temp;
+        for(int i=0; i<SortedList.size(); i++){     
+            min = SortedList.get(i).temp;
+            minInd = i;      
+            for(int k = i; k < SortedList.size(); k++){
+                if(SortedList.get(k).temp < min){
+                    min = SortedList.get(k).temp;
                     minInd = k;
                 }
             }
-            TransferStore = tempList.get(minInd);
-            tempList.set(i,tempList.get(minInd));
-            tempList.set(minInd,TransferStore); 
+            TransferStore = SortedList.get(i);
+            SortedList.set(i,SortedList.get(minInd));
+            SortedList.set(minInd,TransferStore); 
         }
-
         return SortedList;
     }
-     
+    
+    public int BinarySearch(ArrayList<temps> SortedList, double target){
+        int SearchInd;
+        int start=0, end=SortedList.size()-1;
+        while(start != end){
+            SearchInd=start+end/2;
+            if(SortedList.get(SearchInd).temp == target){
+                return SearchInd;
+            }else if(SortedList.get(SearchInd).temp > target){
+                end = SearchInd;
+            }else{
+                start = SearchInd;
+            }     
+        }
+        return -1;
+    }
+
     public double[][] convertList(ArrayList<temps> givenList){
         double[][] rList = new double [2][givenList.size()];
         for(int i = 0; i<givenList.size(); i++){
